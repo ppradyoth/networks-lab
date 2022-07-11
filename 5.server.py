26bbce3f55c7d12d2d592703daea76eed2dfee28
Pradyoth P <pradyoth.jpg@gmail.com>
2022-07-12 00:06:51 +0530

    Manu at HPE, all others deNIEd

--- 5.server.py
+++ 5.server.py
@@ -4,6 +4,7 @@ port = 5100
 server_socket = socket.socket()
 server_socket.bind((host, port))
 server_socket.listen(2)
+
 conn, address = server_socket.accept()
 print("Connection from: " + str(address))
 while True:
