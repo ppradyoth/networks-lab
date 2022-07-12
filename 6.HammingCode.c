b853580a6ccbdfd5ea2075e12a1d275bae5e0038
Pradyoth P <pradyoth.jpg@gmail.com>
2022-07-12 13:19:57 +0530

    Pass aadhre sakappa

--- 6.HammingCode.c
+++ 6.HammingCode.c
@@ -1,50 +1,86 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int a[4],b[4],r[3],s[3],i,q[3];
-    printf("\nEnter 4 bit data word");
-    for(i=3;i>=0;i--)
+    int data[10], count = 0;
+    int dataatrec[10], c, c1, c2, c3, i;
+
+    printf("Enter 4 bits of data one by one\n");
+    scanf("%d", &data[0]);
+    scanf("%d", &data[1]);
+    scanf("%d", &data[2]);
+    scanf("%d", &data[4]);
+
+    // Calculation of even parity
+    data[6] = data[0] ^ data[2] ^ data[4];
+    data[5] = data[0] ^ data[1] ^ data[4];
+    data[3] = data[0] ^ data[1] ^ data[2];
+
+    printf("\nEncoded data is\n");
+    for (i = 0; i < 7; i++)
+        printf("%d", data[i]);
+    printf("\nTo detect 2 bit errors we need parity bit");
+
+    for (i = 0; i < 7; i++)
     {
-        scanf("%d",&a[i]);
+        if (data[i] == 1)
+            count++;
     }
-    r[0]=(a[2]+a[1]+a[0])%2;
-    r[1]=(a[2]+a[0]+a[3])%2;
-    r[2]=(a[0]+a[1]+a[3])%2;
-    printf("\n7 bit hamming codeword is:\n");
-    for(i=3;i>=0;i--)
+    printf("\ncount\n%d\n", count);
+    data[7] = count % 2;
+    printf("\ndata 7  %d\n", data[7]);
+    printf("\nEncoded data with parity bit is\n");
+    for (i = 0; i < 8; i++)
+        printf("%d", data[i]);
+    count = 0;
+    printf("\n\nEnter received data bits one by one\n");
+    for (i = 0; i < 8; i++)
     {
-        printf("%d\t",a[i]);
+        scanf("%d", &dataatrec[i]);
+        if (dataatrec[i] == 1)
+            count++;
     }
-    for(i=2;i>=0;i--)
+    int p;
+    p = count % 2;
+    c1 = dataatrec[6] ^ dataatrec[4] ^ dataatrec[2] ^ dataatrec[0];
+    c2 = dataatrec[5] ^ dataatrec[4] ^ dataatrec[1] ^ dataatrec[0];
+    c3 = dataatrec[3] ^ dataatrec[2] ^ dataatrec[1] ^ dataatrec[0];
+    c = c3 * 4 + c2 * 2 + c1;
+
+    if (c == 0)
     {
-        printf("%d\t",r[i]);
+        if (p == 0)
+            printf("\nNo error while transmission of data\n");
+        else
+            printf("\nError in parity bit");
     }
-    printf("\n");
-    printf("\n Enter the 4 bit received word:");
-    for (i=3;i>=0;i--)
+    else
     {
-        scanf("%d",&b[i]);
+        if (p == 1)
+            printf("\nError on position %d", c);
+        else
+        {
+            printf("\n2 bit error cannot be fixed");
+            return 0;
+        }
+        printf("\nData sent : ");
+        for (i = 0; i < 7; i++)
+            printf("%d", data[i]);
+
+        printf("\nData received : ");
+        for (i = 0; i < 7; i++)
+            printf("%d", dataatrec[i]);
+        printf("\nCorrect message is\n");
+
+        // if errorneous bit is 0 we complement it else vice versa
+        if (dataatrec[7 - c] == 0)
+            dataatrec[7 - c] = 1;
+        else
+            dataatrec[7 - c] = 0;
+        for (i = 0; i < 7; i++)
+        {
+            printf("%d", dataatrec[i]);
+        }
     }
-    s[0]=(b[2]+b[1]+b[0]+r[0])%2;
-    s[1]=(b[3]+b[2]+b[1]+r[1])%2;
-    s[2]=(b[0]+b[1]+b[3]+r[2])%2;
-    printf("\n Syndrome is \n");
-    for(i=2;i>=0;i--)
-    {
-        printf("%d\t",s[i]);
-    }
-    printf("\n");
-    if((s[2]==0)&&(s[1]==0)&&(s[0]==0))
-    {
-        printf("\nNo error detected");
-    }
-    if((s[2]==1)&&(s[1]==0&&s[0]==1))
-        printf("\nError detected in 1 bit (b0) position from right\n");
-    if((s[2]==1)&&(s[1]==1)&&(s[0]==1))
-        printf("\nError detected in 2 bit (b1) position from right\n");
-    if((s[2]==0)&&(s[1]==1)&&(s[0]==1))
-        printf("\nError detected in 3 bit (b2) position from right\n");
-    if((s[2]==1)&&(s[1]==1)&&(s[0]==0))
-        printf("\nError detected in 4 bit (b3) position from right\n");
-    return 1;
+    return 0;
 }
